05e7b02c9d7ae58935b110c3bae12073209ca86a
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-10-02 22:52:57 +0700

    add: Sizing widgets

--- codelab06_flutter/basic_layout_flutter/lib/main.dart
+++ codelab06_flutter/basic_layout_flutter/lib/main.dart
@@ -15,28 +15,52 @@ class MyApp extends StatelessWidget {
       title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter layout demo')),
-        body: Center(child: buildColumn()),
+        body: Center(child: packingWiget()),
       ),
     );
   }
 
-  Widget buildRow() =>
-    Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Image.asset('images/djawatan.jpg', width: 100, height: 100, fit: BoxFit.cover),
-        Image.asset('images/cabai.jpg', width: 100, height: 100, fit: BoxFit.cover),
-        Image.asset('images/djawatan.jpg', width: 100, height: 100, fit: BoxFit.cover),
-      ],
-    );
+  Widget packingWiget() => Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Icon(Icons.star, color: Colors.green[500]),
+      Icon(Icons.star, color: Colors.green[500]),
+      Icon(Icons.star, color: Colors.green[500]),
+      const Icon(Icons.star, color: Colors.black),
+      const Icon(Icons.star, color: Colors.black),
+    ],
+  );
 
-  Widget buildColumn() =>
-    Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Image.asset('images/djawatan.jpg', width: 100, height: 100, fit: BoxFit.cover),
-        Image.asset('images/cabai.jpg', width: 100, height: 100, fit: BoxFit.cover),
-        Image.asset('images/djawatan.jpg', width: 100, height: 100, fit: BoxFit.cover),
-      ],
-    );
+  Widget buildRow() => Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Expanded(child: Image.asset('images/djawatan.jpg')),
+      Expanded(flex: 2, child: Image.asset('images/cabai.jpg')),
+      Expanded(child: Image.asset('images/djawatan.jpg')),
+    ],
+  );
+
+  Widget buildColumn() => Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Image.asset(
+        'images/djawatan.jpg',
+        width: 100,
+        height: 100,
+        fit: BoxFit.cover,
+      ),
+      Image.asset(
+        'images/cabai.jpg',
+        width: 100,
+        height: 100,
+        fit: BoxFit.cover,
+      ),
+      Image.asset(
+        'images/djawatan.jpg',
+        width: 100,
+        height: 100,
+        fit: BoxFit.cover,
+      ),
+    ],
+  );
 }
