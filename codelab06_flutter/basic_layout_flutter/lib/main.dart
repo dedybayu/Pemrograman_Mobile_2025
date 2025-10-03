63b8a6159c10fac6112d716dc83c951a0657e614
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-10-03 21:02:29 +0700

    add: container

--- codelab06_flutter/basic_layout_flutter/lib/main.dart
+++ codelab06_flutter/basic_layout_flutter/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  debugPaintSizeEnabled = false; // Set to true for visual layout.
+  debugPaintSizeEnabled = false; 
   runApp(const MyApp());
 }
 
@@ -12,138 +12,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Galery Dedy Bayu (2341720041)',
-      home: buildHomePage('Galery Squidward Tentacles'),
-    );
-  }
-
-  Widget buildHomePage(String title) {
-    const titleText = Padding(
-      padding: EdgeInsets.all(20),
-      child: Text(
-        'Tampan dan Berani',
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          letterSpacing: 0.5,
-          fontSize: 30,
-        ),
-      ),
-    );
-
-    const subTitle = Text(
-      '"Tampan dan Berani" adalah lukisan karya Squidward Tentacles. '
-      'Sering digambarkan tampan dan berani, meski pernah diejek '
-      'sebagai “lebih cocok masuk tempat sampah”.',
-      textAlign: TextAlign.center,
-      style: TextStyle(fontFamily: 'Georgia', fontSize: 25),
-    );
-
-    final stars = Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(Icons.star, color: Colors.yellow[800]),
-        Icon(Icons.star, color: Colors.yellow[800]),
-        Icon(Icons.star, color: Colors.yellow[800]),
-        const Icon(Icons.star, color: Colors.black),
-        const Icon(Icons.star, color: Colors.black),
-      ],
-    );
-
-    final ratings = Container(
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          stars,
-          const Text(
-            '170 Reviews',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20,
-            ),
-          ),
-        ],
-      ),
-    );
-
-    const descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 18,
-      height: 2,
-    );
-
-    final iconList = DefaultTextStyle.merge(
-      style: descTextStyle,
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                Icon(Icons.brush, color: Colors.blue[500]), // ikon seni
-                const Text('SENI:'),
-                const Text('Abstrak'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(
-                  Icons.star,
-                  color: Colors.amber[700],
-                ), // ikon bintang = ketampanan
-                const Text('NILAI:'),
-                const Text('100/100'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(
-                  Icons.security,
-                  color: Colors.red[400],
-                ), // ikon perisai = keberanian
-                const Text('BRAVE:'),
-                const Text('MAX'),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-
-    final leftColumn = Container(
-      padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
-      child: Column(children: [titleText, subTitle, ratings, iconList]),
-    );
-
-    final mainImage = Image.asset(
-      'images/tampan_dan_berani.png',
-      fit: BoxFit.cover,
-    );
-
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.fromLTRB(0, 40, 0, 30),
-          height: 600,
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(width: 440, child: leftColumn),
-                mainImage,
-              ],
-            ),
-          ),
-        ),
+      title: 'Dedy Bayu Setiawan (2341720041)',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Container')),
+        body: Center(child: _buildImageColumn()),
       ),
     );
   }
+
+  Widget _buildImageColumn() {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.black26),
+      child: Column(children: [_buildImageRow(1), _buildImageRow(3)]),
+    );
+  }
+
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 10, color: Colors.black38),
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+      ),
+      margin: const EdgeInsets.all(4),
+      child: AspectRatio(
+        aspectRatio: 16 / 9, // Rasio
+        child: Image.asset(
+          'images/pic$imageIndex.jpg',
+          fit: BoxFit.contain, // Fit
+        ),
+      ),
+    ),
+  );
+
+  Widget _buildImageRow(int imageIndex) => Row(
+    children: [
+      _buildDecoratedImage(imageIndex),
+      _buildDecoratedImage(imageIndex + 1),
+    ],
+  );
 }
