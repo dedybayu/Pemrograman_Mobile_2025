9895fe759587dfcd3bc39e7be14fefefc88cbed6
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-10-03 21:30:41 +0700

    add: listview

--- codelab06_flutter/basic_layout_flutter/lib/main.dart
+++ codelab06_flutter/basic_layout_flutter/lib/main.dart
@@ -2,52 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  debugPaintSizeEnabled = false; 
+  debugPaintSizeEnabled = false;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const showGrid = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dedy Bayu Setiawan (2341720041)',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Container')),
-        body: Center(child: _buildImageColumn()),
+        appBar: AppBar(title: const Text('List Wisata di Banyuwangi')),
+        body: Center(child: showGrid ? _buildGrid() : _buildList()),
       ),
     );
   }
 
-  Widget _buildImageColumn() {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.black26),
-      child: Column(children: [_buildImageRow(1), _buildImageRow(3)]),
+  Widget _buildGrid() => GridView.count(
+    crossAxisCount: 3,
+    padding: const EdgeInsets.all(4),
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    children: _buildGridTileList(12),
+  );
+
+  List<Widget> _buildGridTileList(int count) =>
+      List.generate(count, (i) => Image.asset('images/pic$i.jpg'));
+
+  Widget _buildList() {
+    return ListView(
+      children: [
+        _tile('Pantai Pulau Merah', 'Pantai dengan pasir merah & sunset indah', Icons.beach_access),
+        _tile('Kawah Ijen', 'Fenomena blue fire yang mendunia', Icons.landscape),
+        _tile('Pantai Plengkung (G-Land)', 'Surga bagi peselancar internasional', Icons.surfing),
+        _tile('Taman Nasional Baluran', 'Dikenal sebagai “Africa van Java”', Icons.park),
+        _tile('Teluk Hijau', 'Air laut hijau jernih & pasir putih', Icons.water),
+        const Divider(),
+        _tile('Pantai Boom', 'Dekat kota, sering untuk festival & konser', Icons.festival),
+        _tile('Air Terjun Jagir', 'Dijuluki “air terjun kembar”', Icons.waterfall_chart),
+        _tile('Pantai Wedi Ireng', 'Pantai eksotis yang masih alami', Icons.nature),
+        _tile('Pantai Sukamade', 'Habitat penyu, bisa lihat pelepasan tukik', Icons.pets),
+        _tile('De Djawatan Benculuk', 'Hutan trembesi mirip setting film Lord of the Rings', Icons.forest),
+      ],
     );
   }
 
-  Widget _buildDecoratedImage(int imageIndex) => Expanded(
-    child: Container(
-      decoration: BoxDecoration(
-        border: Border.all(width: 10, color: Colors.black38),
-        borderRadius: const BorderRadius.all(Radius.circular(8)),
+  ListTile _tile(String title, String subtitle, IconData icon) {
+    return ListTile(
+      title: Text(
+        title,
+        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
       ),
-      margin: const EdgeInsets.all(4),
-      child: AspectRatio(
-        aspectRatio: 16 / 9, // Rasio
-        child: Image.asset(
-          'images/pic$imageIndex.jpg',
-          fit: BoxFit.contain, // Fit
-        ),
-      ),
-    ),
-  );
-
-  Widget _buildImageRow(int imageIndex) => Row(
-    children: [
-      _buildDecoratedImage(imageIndex),
-      _buildDecoratedImage(imageIndex + 1),
-    ],
-  );
+      subtitle: Text(subtitle),
+      leading: Icon(icon, color: Colors.blue[500]),
+    );
+  }
 }
